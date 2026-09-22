9223015bc5b3a69d4dc712693f08368e68faaaaf
Marketingbende <info@marketingbende.nl>
2026-09-22 10:22:00 +0000

    chore: bundel van e390c03

---
+++ assets/wifi-zero-DQJNxJOL.js
@@ -0,0 +1,1 @@
+import{bA as o}from"./index-o1ZnDTWR.js";import"./react-vendor-BpwM1LRX.js";import"./dnd-vendor-kUuqBB18.js";import"./supabase-vendor-Dax_0g8U.js";import"./import-export-vendor-B-TnuAQX.js";import"./image-vendor-RfDGl-J6.js";import"./markdown-vendor-CB3HMLSg.js";const i=[["path",{d:"M12 20h.01",key:"zekei9"}]],d=o("wifi-zero",i);export{i as __iconNode,d as default};
