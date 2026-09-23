b93a97fd83efbdca965efd7466e5278adb91f560
Marketingbende <info@marketingbende.nl>
2026-09-23 21:06:28 +0000

    chore: bundel van de6b2c8

--- assets/tally-1-DrI5yn1V.js
+++
@@ -1,1 +0,0 @@
-import{bL as o}from"./index-DDJ9OL9T.js";import"./react-vendor-BpwM1LRX.js";import"./dnd-vendor-kUuqBB18.js";import"./supabase-vendor-Dax_0g8U.js";import"./import-export-vendor-B-TnuAQX.js";import"./image-vendor-RfDGl-J6.js";import"./markdown-vendor-CB3HMLSg.js";const t=[["path",{d:"M4 4v16",key:"6qkkli"}]],l=o("tally-1",t);export{t as __iconNode,l as default};
